1fb3d4ac54d541b2ecb68b5e1e4fcb228f0d8318
requiem-of-zero <swonggzz@gmail.com>
2022-09-24 22:12:46 -0700

    Fixed arrow alignment

--- src/components/CoverCarousel/CoverCarousel.styles.js
+++ src/components/CoverCarousel/CoverCarousel.styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CarouselWrapper = styled.div`
   margin: 0 auto;
@@ -7,19 +7,24 @@ export const CarouselWrapper = styled.div`
   .splide {
     padding: 10px 48px 0 48px;
   }
-  h2{
+  h2 {
     margin: 0;
-    span{
-        font-weight: 400;
+    span {
+      font-weight: 400;
     }
   }
+
   svg {
     fill: #bdbcbc;
   }
 
-  .splide__arrow:hover{
+  .splide__arrow {
+    top: calc(50% - 23px);
+  }
+  
+  .splide__arrow:hover {
     svg {
-        fill: black;
+      fill: black;
     }
   }
 `;
